709cfa77c312515759832b21169ffdca7bcca682
bgray01 <107816284+bgray01@users.noreply.github.com>
2025-10-05 14:18:11 -0700

    Create LocationClass.cs

---
+++ LocationClass.cs
@@ -0,0 +1,11 @@
+namespace Location_Inquiry
+{
+    public class Location
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Status { get; set; }
+        public int Inventory { get; set; }
+    }
+}
